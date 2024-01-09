82fc6370250a790e7ad53849a045e2c02cdde269
stevenssebuma <sse23007@byui.edu>
2024-01-09 20:58:18 +0300

    Added content

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string answer = Console.ReadLine();
+        int percent = int.Parse(answer);
+
+        string letter = "";
+
+        if (percent >= 97)
+        {
+            letter = "A+";
+        }
+        else if (percent >= 93 && percent < 97)
+        {
+            letter = "A";
+        }
+        else if (percent >= 90 && percent < 93)
+        {
+            letter = "A-";
+        }
+        else if (percent >=87 && percent < 90)
+        {
+            letter = "B+";
+        }
+        else if (percent >= 83 && percent < 87)
+        {
+            letter = "B";
+        }
+        else if (percent >= 80 && percent < 84)
+        {
+            letter = "B-";
+        }
+        else if (percent >= 77 && percent < 80)
+        {
+            letter = "C+";
+        }
+         else if (percent >= 73 && percent < 77)
+        {
+            letter = "C";
+        }
+         else if (percent >= 70 && percent < 73)
+        {
+            letter = "C-";
+        }
+         else if (percent >= 67 && percent < 70)
+        {
+            letter = "D+";
+        }
+         else if (percent >= 65 && percent < 67)
+        {
+            letter = "D";
+        }
+        else{
+            letter = "F";
+
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+        
+        if (percent >= 70)
+        {
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!!!");
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("This is in C#.");
+
+        int number = 5;
+        number = 6;
+        number = 9;
+        Console.WriteLine($"{number}");
+
+        Console.Write("What is your name? ");
+        string name = Console.ReadLine();
+        Console.WriteLine(name);
+        
     }
 }
