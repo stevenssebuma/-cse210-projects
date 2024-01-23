759adf204de67e41f18af26484e1b578827a3388
stevenssebuma <sse23007@byui.edu>
2024-01-23 11:52:43 +0300

    Added Content

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -1,51 +1,16 @@
 using System;
-
-// class Program
-// {
-//     static void Main(string[] args)
-//     {
-//         Console.WriteLine("Hello World!");
-//         Console.WriteLine("This is in C#.");
-
-//         int number = 5;
-//         number = 6;
-//         number = 9;
-//         Console.WriteLine($"{number}");
-
-//         Console.Write("What is your name? ");
-//         string name = Console.ReadLine();
-//         Console.WriteLine(name);
-        
-//     }
-// }
-
-
-    // A code template for the category of things known as Person. The
-    // responsibility of a Person is to hold and display personal information.
-    public class Person
+ 
+class Program
+{
+    static void Main(string[] args)
     {
-        // The C# convention is to start member variables with an underscore _
-        public string _givenName = "Steven";
-        public string _familyName = "Ssebuma";
+        Person person = new Person();
+        person._firstName = "Steven";
+        person._lastName = "Ssebuma";
+        person._age = 24;
 
-        // A special method, called a constructor that is invoked using the  
-        // new keyword followed by the class name and parentheses.
-        public Person()
-        {
-        }
-
-        // A method that displays the person's full name as used in eastern 
-        // countries or <family name, given name>.
-        public void ShowEasternName()
-        {
-            Console.WriteLine($"{_familyName}, {_givenName}");
-        }
-
-        // A method that displays the person's full name as used in western 
-        // countries or <given name family name>.
-        public void ShowWesternName()
-        {
-            Console.WriteLine($"{_givenName} {_familyName}");
-        }
+        string _fullName = person._firstName + " " + person._lastName;
+        Console.WriteLine("Full name:" + _fullName );
+     
     }
-    
+}
