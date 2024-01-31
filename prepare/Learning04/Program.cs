363cc9e269c255585a395e462aca95eb2c96d5a3
stevenssebuma <sse23007@byui.edu>
2024-01-31 13:10:15 +0300

    Added Content

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Steven", "Programming");
+        
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Jonathan Aloya", "Fractions", "7.9", "9-9");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Dennis", "Public Health", "Enviromental Awareness");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInfo());
     }
 }
