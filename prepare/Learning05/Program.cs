78b85c094f24696f509430d40b77b8e44b5e296c
stevenssebuma <sse23007@byui.edu>
2024-02-07 17:39:41 +0300

    Added more content

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -5,28 +5,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Notice that the list is a list of "Shape" objects. That means
-        // you can put any Shape objects in there, and also, any object where
-        // the class inherits from Shape
         List<Shape> shapes = new List<Shape>();
 
-        Square s1 = new Square("Red", 3);
-        shapes.Add(s1);
+        Square shape1 = new Square("Red", 7);
+        shapes.Add(shape1);
 
-        Rectangle s2 = new Rectangle("Blue", 4, 5);
-        shapes.Add(s2);
+        Rectangle shape2 = new Rectangle("Blue", 14, 9;
+        shapes.Add(shape2);
 
-        Circle s3 = new Circle("Green", 6);
-        shapes.Add(s3);
+        Circle shape3 = new Circle("Green", 3);
+        shapes.Add(shape3);
 
-        foreach (Shape s in shapes)
+        foreach (Shape shape in shapes)
         {
-            // Notice that all shapes have a GetColor method from the base class
-            string color = s.GetColor();
+            
+            string color = shape.GetColor();
 
-            // Notice that all shapes have a GetArea method, but the behavior is
-            // different for each type of shape
-            double area = s.GetArea();
+            double area = shape.GetArea();
 
             Console.WriteLine($"The {color} shape has an area of {area}.");
         }
